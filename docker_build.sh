3f19c2be81a1b3ba730c7dfc4a613b922eba3f1c
jarek <iaroslav.pavlov@unibas.ch>
2025-01-21 12:23:19 +0100

    changing docker build file

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build --rm -f ./Dockerfile -t gitlab.switch.ch/ub-unibas/dlza/microservices/dlza-manager-clerk:latest .
-docker tag gitlab.switch.ch/ub-unibas/dlza/microservices/dlza-manager-clerk:latest registry.localhost:5001/gitlab.switch.ch/ub-unibas/dlza/microservices/dlza-manager-clerk
-docker push registry.localhost:5001/gitlab.switch.ch/ub-unibas/dlza/microservices/dlza-manager-clerk
+docker build --rm -f ./Dockerfile -t gitlab.switch.ch/ub-unibas/dlza/microservices/dlza-miniresolver/dlza-manager-clerk:latest .
+docker tag gitlab.switch.ch/ub-unibas/dlza/microservices/dlza-miniresolver/dlza-manager-clerk:latest registry.localhost:5001/gitlab.switch.ch/ub-unibas/dlza/microservices/dlza-miniresolver/dlza-manager-clerk
+docker push registry.localhost:5001/gitlab.switch.ch/ub-unibas/dlza/microservices/dlza-miniresolver/dlza-manager-clerk
