e345d8414d4b7f17d0681d20f7d045988c30c371
jarek <iaroslav.pavlov@unibas.ch>
2025-04-10 12:39:47 +0200

    Getting new handler objects for ona

--- controller/object_instance_controller.go
+++ controller/object_instance_controller.go
@@ -13,6 +13,7 @@ type ObjectInstanceController struct {
 
 func (o *ObjectInstanceController) InitRoutes(StorageInfoRouter *gin.RouterGroup) {
 	StorageInfoRouter.GET("/:name", o.ObjectInstanceWithNameExists)
+	StorageInfoRouter.GET("/alias-and-location/:alias/:locations-name", o.GetObjectInstancesBySignatureAndLocationsPathName)
 }
 
 func (o *ObjectInstanceController) Path() string {
@@ -41,4 +42,24 @@ func (o *ObjectInstanceController) ObjectInstanceWithNameExists(ctx *gin.Context
 		return
 	}
 	ctx.JSON(http.StatusOK, objectInstances)
+}
+
+// GetObjectInstancesBySignatureAndLocationsPathName godoc
+// @Summary		Getting object instance by alias of object and storage locations name
+// @Description	Getting object instance by alias of object and storage locations name
+// @Security 	ApiKeyAuth
+// @ID 			object-instance-by-alias-and-locations-name
+// @Produce		json
+// @Success		200
+// @Failure 	400
+// @Router		/object-instance/alias-and-location/{alias}/{locations-name} [get]
+func (o *ObjectInstanceController) GetObjectInstancesBySignatureAndLocationsPathName(ctx *gin.Context) {
+	alias := ctx.Param("alias")
+	locationsName := ctx.Param("locations-name")
+	objectInstance, err := o.ClientClerkHandlerService.GetObjectInstancesBySignatureAndLocationsPathName(ctx, &pb.AliasAndLocationsName{Alias: alias, LocationsName: locationsName})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "request failed"})
+		return
+	}
+	ctx.JSON(http.StatusOK, objectInstance)
 }
