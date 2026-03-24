4f5aaddf9e1a00220497b494b66cf5d18b03c44b
jarek <iaroslav.pavlov@unibas.ch>
2026-03-24 11:01:28 +0100

    Updates regarding the dismantling of the temporary storage

--- controller/object_instance_controller.go
+++ controller/object_instance_controller.go
@@ -1,10 +1,11 @@
 package controller
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	pbHandler "github.com/ocfl-archive/dlza-manager-handler/handlerproto"
 	pb "github.com/ocfl-archive/dlza-manager/dlzamanagerproto"
-	"net/http"
 )
 
 type ObjectInstanceController struct {
@@ -14,6 +15,7 @@ type ObjectInstanceController struct {
 func (o *ObjectInstanceController) InitRoutes(StorageInfoRouter *gin.RouterGroup) {
 	StorageInfoRouter.GET("/:name", o.ObjectInstanceWithNameExists)
 	StorageInfoRouter.GET("/signature-and-location/:signature/:locations-name", o.GetObjectInstancesBySignatureAndLocationsPathName)
+	StorageInfoRouter.GET("/raw-check/:object-id", o.GetRawObjectInstanceByObjectId)
 }
 
 func (o *ObjectInstanceController) Path() string {
@@ -57,6 +59,25 @@ func (o *ObjectInstanceController) GetObjectInstancesBySignatureAndLocationsPath
 	signature := ctx.Param("signature")
 	locationsName := ctx.Param("locations-name")
 	objectInstance, err := o.ClientClerkHandlerService.GetObjectInstancesBySignatureAndLocationsPathName(ctx, &pb.AliasAndLocationsName{Alias: signature, LocationsName: locationsName})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "request failed"})
+		return
+	}
+	ctx.JSON(http.StatusOK, objectInstance)
+}
+
+// GetRawObjectInstanceByObjectId godoc
+// @Summary	Getting object instance by object ID if it has raw status
+// @Description	Getting object instance by object ID if it has raw status
+// @Security	ApiKeyAuth
+// @ID	check-raw-object-instance-by-object-id
+// @Produce	json
+// @Success	200
+// @Failure	400
+// @Router	/object-instance/raw-check/{object-id} [get]
+func (o *ObjectInstanceController) GetRawObjectInstanceByObjectId(ctx *gin.Context) {
+	objectId := ctx.Param("object-id")
+	objectInstance, err := o.ClientClerkHandlerService.CheckRawObjectInstanceByObjectId(ctx, &pb.Id{Id: objectId})
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "request failed"})
 		return
